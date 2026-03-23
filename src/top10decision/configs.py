a7aa7f71f3f776821cf752c62c8d322f9fb4da80
njedu2023-prog <njedu2023@gmail.com>
2026-03-24 06:10:39 +0800

    configs: add entry proxy + eret truth config

--- src/top10decision/configs.py
+++ src/top10decision/configs.py
@@ -23,3 +23,23 @@ class FillTruthConfig:
 
 
 fill_truth_config = FillTruthConfig()
+
+@dataclass(frozen=True)
+class EntryPriceProxyConfig:
+    mode_default: str = "t1_open"
+    mode_fallback: str = "t1_open_degrade_missing"
+
+entry_price_proxy_config = EntryPriceProxyConfig()
+
+
+@dataclass(frozen=True)
+class ERetTruthConfig:
+    entry_price_proxy_mode: str = "t1_open"
+    sell_price_col: str = "close_t2"
+    output_path_template: str = "data/market/eret_truth_{trade_date}.csv"
+
+    label_quality_default: str = "strong"
+    label_quality_missing_truth: str = "weak_missing_truth"
+
+
+eret_truth_config = ERetTruthConfig()
